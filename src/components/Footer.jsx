8ab5351563e872c0696af4c8c425c26bc5133a8b
eugeniayu99 <eugeniayu99@gmail.com>
2026-02-09 21:47:01 +0900

    푸터수정완료

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,19 @@
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer id="footer" role="contentinfo">
+        <div className="footer__inner">
+          <p className="footer__text">
+            &copy; 2026 Choi Yujin. All rights reserved.<br />
+            본 사이트는 React를 기반으로 제작된 개인 포트폴리오입니다.<br />
+            Microsoft Edge 및 Chrome 최신 버전에 최적화되어 있습니다.
+          </p>
+          
+        </div>
+    </footer>
   )
 }
 
 export default Footer
+
+
--- src/components/Skip.jsx
+++ src/components/Skip.jsx
@@ -4,11 +4,10 @@ const Skip = () => {
   return (
     <div id="skip">
         <a href="#header">헤더영역바로가기</a>
-        <a href="#intro">소개영역바로가기</a>
-        <a href="#skill">스킬영역바로가기</a>
-        <a href="#site">사이트영역바로가기</a>
-        <a href="#port">포트폴리오영역바로가기</a>
-        <a href="#contact">연락처영역바로가기</a>
+        <a href="#intro">인트로영역바로가기</a>
+        <a href="#about">소개영역바로가기</a>
+        <a href="#work">실무프로젝트영역바로가기</a>
+        <a href="#portfolio">개인포트폴리오영역바로가기</a>
     </div>
   )
 }
